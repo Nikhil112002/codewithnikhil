5cce6be8f8674d5088a93a9661db4687ad71019a
Nikhil112002 <nikhilnsharma.01@gmail.com>
2023-04-17 17:23:16 +0530

    codewithnikhil

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -79,7 +79,7 @@ const Contact = () => {
             </p>
           </div>
           <div class="col-md-5 my-5">
-            <form class="row g-3" action="https://formspree.io/f/xjvdpqjk" method="POST" onSubmit={handleSubmit}>
+            <form class="row g-3" action="https://formspree.io/f/xjvdpqjk" method="POST">
               <div class="col-md-12">
                 <label for="inputEmail4" class="form-label labelClr">
                   Full Name 
